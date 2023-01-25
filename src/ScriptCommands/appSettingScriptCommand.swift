88903775c4a3a20cea115cd3a2409f99f4d98390
steventheworker <steventheworker@gmail.com>
2023-01-24 21:23:01 -0800

    localizedString -> rawValue

--- src/ScriptCommands/appSettingScriptCommand.swift
+++ src/ScriptCommands/appSettingScriptCommand.swift
@@ -6,12 +6,12 @@ class appSettingScriptCommand: NSScriptCommand {
         let named = self.evaluatedArguments!["named"] as! String
         let shortcutIndex = (Preferences.appsToShow[0] == .active) ? 0 : Preferences.appsToShow.count - 1 // if shortcut 1 = active, use it, else use last shortcut
         var val = ""
-        if (named == "appsToShow") {val = Preferences.appsToShow[shortcutIndex].localizedString}
-        else if (named == "showHiddenWindows") {val = Preferences.showHiddenWindows[shortcutIndex].localizedString}
-        else if (named == "showFullscreenWindows") {val = Preferences.showFullscreenWindows[shortcutIndex].localizedString}
-        else if (named == "showMinimizedWindows") {val = Preferences.showMinimizedWindows[shortcutIndex].localizedString}
-        else if (named == "spacesToShow") {val = Preferences.spacesToShow[shortcutIndex].localizedString}
-        else if (named == "screensToShow") {val = Preferences.screensToShow[shortcutIndex].localizedString}
+        if (named == "appsToShow") {val = Preferences.appsToShow[shortcutIndex].rawValue}
+        else if (named == "showHiddenWindows") {val = Preferences.showHiddenWindows[shortcutIndex].rawValue}
+        else if (named == "showFullscreenWindows") {val = Preferences.showFullscreenWindows[shortcutIndex].rawValue}
+        else if (named == "showMinimizedWindows") {val = Preferences.showMinimizedWindows[shortcutIndex].rawValue}
+        else if (named == "spacesToShow") {val = Preferences.spacesToShow[shortcutIndex].rawValue}
+        else if (named == "screensToShow") {val = Preferences.screensToShow[shortcutIndex].rawValue}
         else if (named == "showTabsAsWindows") {val = Preferences.showTabsAsWindows ? "1" : "0"}
         return val
     }
