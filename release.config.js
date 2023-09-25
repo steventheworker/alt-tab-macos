6a37dce6baa7e9f58bc9c6a83134e31df9cc009c
steventheworker <steventheworker@gmail.com>
2023-09-24 22:19:51 -0700

    feat(core): fix travis (3)
    travis & semantic-release use scriptable branch (opposed to master)

--- release.config.js
+++ release.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
+        branches: ['scriptable'], // Specify the branch you want to use for releases
         ['@semantic-release/commit-analyzer', {
             'preset': 'angular',
             // having version increments and builds for these commit types is valuable
